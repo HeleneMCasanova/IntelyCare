26704658d7863e7b8726e51c1f5660ba4002b899
Casanova <casanovah@wit.edu>
2019-02-07 23:22:10 -0500

    Updating code; Now goes through the excel and checks correlation coefficient

--- correlation_coefficient.py
+++ correlation_coefficient.py
@@ -1,76 +1,137 @@
 import numpy as np
 import datetime as dt
+from collections import OrderedDict
 import csv
+
+def getLists(read_csv, g):
+    list_one = []
+    for row in read_csv:
+        list_one.append(row[g])
+    
+    return list_one
+
+def printResults(list_one, list_two):
+    list_one = np.asarray(list_one).astype(np.float)
+    list_two = np.asarray(list_two).astype(np.float)
+    correlation = np.corrcoef(list_one, list_two)[0,1]
+    print(correlation)
+    return 0
+
+def tg(list_one):
+    newList = []
+    for time in list_one:
+        if time == 'ET':
+            newList.append(0)
+        elif time == 'DT':
+            newList.append(1)
+        elif time == 'WDT':
+            newList.append(2)
+        elif time == 'WET':
+            newList.append(3)
+        elif time == 'NT':
+            newList.append(4)
+        else:
+            newList.append(5)
+    #print(newList)
+    return newList
+
+def ipt(list_one):
+    newList = []
+    for t in list_one:
+        if t == 'Nurse':
+            newList.append(0)
+        elif t == 'RN':
+            newList.append(1)
+        elif t == 'CNA':
+            newList.append(2)
+        else:
+            newList.append(3)
+    return newList
+
+def fs(list_one):
+    newList = []
+    for t in list_one:
+        if t == 'Deleted':
+            newList.append(0)
+        elif t == 'Requested':
+            newList.append(1)
+        else:
+            newList.append(2)
+    return newList
+
+def state(list_one):
+    newList = []
+    for t in list_one:
+        if t == 'MA':
+            newList.append(0)
+        elif t == 'PA':
+            newList.append(1)
+        elif t == 'OH':
+            newList.append(2)
+        else:
+            newList.append(3)
+    return newList
+
+def market(list_one):
+    newList = []
+    for t in list_one:
+        if t == 'BOS-Cape':
+            newList.append(0)
+        elif t == 'BOS-North':
+            newList.append(1)
+        elif t == 'BOS-South':
+            newList.append(2)
+        elif t == 'BOS-West':
+            newList.append(3)
+        elif t == 'CVG-1':
+            newList.append(4)
+        elif t == 'PHL-1':
+            newList.append(5)
+        elif t == 'PIT-1':
+            newList.append(6)
+        else:
+            newList.append(7)
+    return newList
+
+def price(list_one):
+    newList = []
+    for t in list_one:
+        if t != '':
+            newList.append(t)
+        else:
+            newList.append(0)
+    return newList
 
 
 list_one = []
 list_two = []
 
+groups = ['RequestHours', 'TimeGroup', 'ShiftTimeID','IntelyProType', 'LastMinuteShift', 'Holiday', 'HolidayBilling', 'FinalStatus', 'ClientID', 'Market','State', 'ZipCode','CNA', 'LPN', 'RN', 'UserGroup', 'Price']
 
-
-with open('rate_overview_sample.csv') as csvfile:
+with open('IntelyCareData.csv') as csvfile:
     read_csv = csv.DictReader(csvfile, delimiter=',')
+    list_two = getLists(read_csv, 'newDuration')
     
-    for row in read_csv:
-        s = row['DateCreated']
-        slashCount = 0
-        year = ''
-        month = ''
-        day = ''
-        hour = ''
-        min = ''
-        for c in s:
-            if c == '/' or c == ' ' or c == ':':
-                slashCount += 1
-            else:
-                if slashCount == 0:
-                    month += c
-                elif slashCount == 1:
-                    day += c
-                elif slashCount == 2:
-                    year += c
-                elif slashCount == 3:
-                    hour += c
-                elif slashCount == 4:
-                    min += c
-        start = dt.datetime(year=int(year) + 2000, month=int(month), day=int(day), hour=int(hour), minute=int(min))
+    for g in groups:
+        csvfile.seek(0)
+        print(g)
 
-        s = row['FinalStatusTime']
-        slashCount = 0
-        year = ''
-        month = ''
-        day = ''
-        hour = ''
-        min = ''
-        for c in s:
-            if c == '/' or c == ' ' or c == ':':
-                slashCount += 1
-            else:
-                if slashCount == 0:
-                    month += c
-                elif slashCount == 1:
-                    day += c
-                elif slashCount == 2:
-                    year += c
-                elif slashCount == 3:
-                    hour += c
-                elif slashCount == 4:
-                    min += c
-        end = dt.datetime(year=int(year) + 2000, month=int(month), day=int(day), hour=int(hour), minute=int(min))
+        list_one = getLists(read_csv, g)
+        list_one.pop(0)
+        
+        if g == 'TimeGroup':
+            list_one = tg(list_one)
+        if g == 'IntelyProType':
+            list_one = ipt(list_one)
+        if g == 'FinalStatus':
+            list_one = fs(list_one)
+        if g == 'State':
+            list_one = state(list_one)
+        if g == 'Market':
+            list_one = market(list_one)
+        if g == 'Price':
+            list_one = price(list_one)
 
-        list_one.append((end-start).total_seconds())
-        list_two.append(row['ShiftTimeID'])
+        printResults(list_one, list_two)
 
-
-print(len(list_one))
-print(len(list_two))
-
-
-list_one = np.asarray(list_one).astype(np.float)
-list_two = np.asarray(list_two).astype(np.float)
-
-correlation = np.corrcoef(list_one, list_two)[0,1]
-
-
-
-print(correlation)
+        list_one = []
